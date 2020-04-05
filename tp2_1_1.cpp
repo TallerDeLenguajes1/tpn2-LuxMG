d5a6fcc16dd30d820aeb5aef1c8c0855ad2fd0ec
Luz <lucecitagiuliani@gmail.com>
2020-04-05 15:24:27 -0300

    Agregados .gitignore, README.md, y tp2_1_1.cpp

---
+++ tp2_1_1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#define N 4
+#define M 5
+
+int main(){
+	int f,c;
+	double mt[N][M];
+	for(f = 0; f<N; f++){
+		for(f = 0; f<N; f++){
+			printf("%lf ", mt[f][c]);
+		}
+	printf("\n");
+	}
+	return 0;
+}
